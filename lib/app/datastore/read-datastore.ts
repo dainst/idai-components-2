8fa8ba16d122cb9f4c5519f15fb6d6221882c8e9
Daniel <danielmarreirosdeoliveira@googlemail.com>
2016-06-22 13:48:38 +0200

    Write apidoc.

--- lib/app/datastore/read-datastore.ts
+++ lib/app/datastore/read-datastore.ts
@@ -2,17 +2,31 @@ import {Document} from "../core-services/document";
 import {Observable} from "rxjs/Observable";
 
 /**
- * This interface provides read access methods to a datastore
- * maintaining Documents.
+ * The interface providing read access methods 
+ * for datastores supporting the idai-components document model.
+ * For full access see <code>Datastore</code>
  *
  * Implementations guarantee that any of methods declared here
  * have no effect on any of the documents within the datastore.
+ * 
+ * @author Sebastian Cuy
+ * @author Daniel de Oliveira
  */
 export abstract class ReadDatastore  {
 
-    abstract get(id: string): Promise<Document>;
+    /**
+     * @param query
+     * @returns {Promise<T>} resolve -> {Document},
+     *   reject -> the error message or a message key.
+     */ 
+    abstract get(resourceId: string): Promise<Document>;
 
-    abstract find(query: string, options: any): Promise<Document[]>;
+    /**
+     * @param query
+     * @returns {Promise<T>} resolve -> {Document[]},
+     *   reject -> the error message or a message key.
+     */ 
+    abstract find(query: string): Promise<Document[]>;
 
     abstract all(options: any): Promise<Document[]>;
 
