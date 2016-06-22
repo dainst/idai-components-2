aa45ba552a254a20e7c4cc3ed9ba93f1db2655d4
Daniel <danielmarreirosdeoliveira@googlemail.com>
2016-06-22 10:21:21 +0200

    Write apidoc.

--- lib/app/datastore/datastore.ts
+++ lib/app/datastore/datastore.ts
@@ -1,15 +1,23 @@
 import {Document} from "../core-services/document";
 import {ReadDatastore} from "./read-datastore";
 
+/**
+ * The interface for datastores supporting 
+ * the idai-components document model.
+ */ 
 export abstract class Datastore extends ReadDatastore {
 
     abstract create(doc: Document): Promise<string>;
 
-    abstract update(object: Document): Promise<any>;
+    /**
+     * @returns {Promise<T>} resolve -> (),
+     *   reject -> the error message or a message key.
+     */ 
+    abstract update(doc: Document): Promise<any>;
 
-    abstract remove(id: string): Promise<any>;
+    abstract remove(resourceId: string): Promise<any>;
 
     abstract clear(): Promise<any>;
 
-    abstract refresh(id:string):Promise<Document>;
+    abstract refresh(resourceId:string):Promise<Document>;
 }
