eb7c49600263e509099f3429aa835ae1270f981b
Jan G. Wieners <jan@jan-wieners.de>
2016-11-16 15:05:49 +0100

    Add missing bootstrap styles.

--- gulpfile.js
+++ gulpfile.js
@@ -10,74 +10,79 @@ var pkg = require('./package.json');
 var webserver = require('gulp-webserver');
 
 // compile sass and concatenate to single css file in build dir
-gulp.task('convert-sass', function() {
+gulp.task('convert-sass', function () {
 
-	return gulp.src('src/scss/app.scss')
-	  	.pipe(sass({includePaths: [
-			'node_modules/mdbootstrap/sass/',
-			'node_modules/mdi/scss/'
-		], precision: 8}))
-	  	.pipe(concat(pkg.name + '.css'))
-	    .pipe(gulp.dest('src/css'));
+    return gulp.src([
+        'src/scss/app.scss',
+        'node_modules/mdbootstrap/css/bootstrap.css'
+    ])
+        .pipe(sass({
+            includePaths: [
+                'node_modules/mdbootstrap/sass/',
+                'node_modules/mdi/scss/'
+            ], precision: 8
+        }))
+        .pipe(concat(pkg.name + '.css'))
+        .pipe(gulp.dest('src/css'));
 });
 
 function watch() {
     gulp.watch('src/scss/**/*.scss', ['convert-sass']);
 }
 
-gulp.task('webserver-watch', function() {
-	gulp.src('./') 
+gulp.task('webserver-watch', function () {
+    gulp.src('./')
         .pipe(webserver({
-			fallback: 'index.html',
-			port: 8083
-		}));
-	watch();
+            fallback: 'index.html',
+            port: 8083
+        }));
+    watch();
 });
 
 const tscConfig = require('./tsconfig.json');
-gulp.task('compile',['convert-sass'], function () {
+gulp.task('compile', ['convert-sass'], function () {
     // fonts
     gulp.src([
-            'node_modules/mdi/fonts/**/*',
-            'node_modules/mdbootstrap/font/**/*'
-        ])
+        'node_modules/mdi/fonts/**/*',
+        'node_modules/mdbootstrap/font/**/*'
+    ])
         .pipe(gulp.dest('fonts'));
-	
+
     // sources
     gulp
         .src('*.ts')
         .pipe(typescript(tscConfig.compilerOptions))
         .pipe(gulp.dest('./'));
-    
+
     gulp
-		.src('demo/app/**/*.ts')
-		.pipe(typescript(tscConfig.compilerOptions))
-		.pipe(gulp.dest('demo/app/'));
+        .src('demo/app/**/*.ts')
+        .pipe(typescript(tscConfig.compilerOptions))
+        .pipe(gulp.dest('demo/app/'));
 
     gulp
         .src('src/app/**/*.ts')
         .pipe(typescript(tscConfig.compilerOptions))
         .pipe(gulp.dest('src/app/'));
 
-	// test sources
+    // test sources
     return gulp
-		.src('src/test/**/*.ts')
-		.pipe(typescript(tscConfig.compilerOptions))
-		.pipe(gulp.dest('src/test/'));
+        .src('src/test/**/*.ts')
+        .pipe(typescript(tscConfig.compilerOptions))
+        .pipe(gulp.dest('src/test/'));
 });
 
 function createConfig(path) {
-	fs.access(path, fs.F_OK, function (err) {
+    fs.access(path, fs.F_OK, function (err) {
 
-		if (err) {
-			fs.createReadStream(path+'.template').pipe(fs.createWriteStream(path));
-		} else {
-			console.log('Will not create '+path+' from template because file already exists.');
-		}
-	});
+        if (err) {
+            fs.createReadStream(path + '.template').pipe(fs.createWriteStream(path));
+        } else {
+            console.log('Will not create ' + path + ' from template because file already exists.');
+        }
+    });
 }
 
 // Creates config file if it doesn't exist already
 gulp.task('create-configs', function (callback) {
-	createConfig('./demo/config/Configuration.json');
+    createConfig('./demo/config/Configuration.json');
 });
