83b867c76af3aa7a40b9f1279df65f4da5234f7d
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2017-02-27 20:56:27 +0000

    Remove filterSets.

--- datastore.ts
+++ datastore.ts
@@ -1,5 +1,4 @@
 export {Query} from './src/app/datastore/query';
-export {FilterSet} from './src/app/datastore/filterSet';
 export {Datastore} from './src/app/datastore/datastore';
 export {Mediastore} from './src/app/datastore/mediastore';
 export {ReadDatastore} from './src/app/datastore/read-datastore';
--- src/app/datastore/filterSet.ts
+++
@@ -1,3 +0,0 @@
-export interface FilterSet {
-    filters: Array<string>;
-}
