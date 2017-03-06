145cde321f19bf0bd6bf4129d901674121662979
Sebastian Cuy <sebastian.cuy@dainst.de>
2017-03-06 15:24:59 +0100

    refined documentation for query

--- src/app/datastore/query.ts
+++ src/app/datastore/query.ts
@@ -1,8 +1,8 @@
 /**
  * represents a query to the datastore
- * @member q the query string that is used to search documents in the datastore
- * @member type restricts results to the given type
- * @member prefix instead of matching whole tokens (default) treat the query
+ * @property q the query string that is used to search documents in the datastore
+ * @property type restricts results to the given type
+ * @property prefix instead of matching whole tokens (default) treat the query
  *   string as a prefix that has to occur in the tokens of matching documents
  */
 export interface Query {
