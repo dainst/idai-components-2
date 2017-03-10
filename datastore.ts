17b9211266a4c55d93f083919261d5d3084c3c15
Daniel <danielmarreirosdeoliveira@googlemail.com>
2017-03-10 13:17:43 +0000

    Remove mediastore.

--- datastore.ts
+++ datastore.ts
@@ -1,5 +1,3 @@
 export {Query} from './src/app/datastore/query';
 export {Datastore} from './src/app/datastore/datastore';
-export {Mediastore} from './src/app/datastore/mediastore';
 export {ReadDatastore} from './src/app/datastore/read-datastore';
-export {ReadMediastore} from './src/app/datastore/read-mediastore';
