e33d4a14488cdecc0bc51f29b526249469f7ac62
Thomas Kleinke <thomas.kleinke@dainst.de>
2017-05-24 15:17:44 +0200

    export action

--- core.ts
+++ core.ts
@@ -2,3 +2,4 @@
 export {Document} from './src/app/model/document';
 export {Resource} from './src/app/model/resource';
 export {Relations} from './src/app/model/relations';
+export {Action} from './src/app/model/action';
