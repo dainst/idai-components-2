c4a34c0d917d293ea0184186b25fcb3f601439fb
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2017-06-02 13:26:48 +0100

    Revert "Revert "removes js maps""
    This reverts commit 0a59ad86469685e666e22b4d6d56b608ce05181e.

--- idai-field-map.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var idai_field_map_module_1 = require('./src/app/idai-field-map/idai-field-map.module');
-exports.IdaiFieldMapModule = idai_field_map_module_1.IdaiFieldMapModule;
-var map_component_1 = require('./src/app/idai-field-map/map.component');
-exports.MapComponent = map_component_1.MapComponent;
-var map_state_1 = require('./src/app/idai-field-map/map-state');
-exports.MapState = map_state_1.MapState;
-//# sourceMappingURL=idai-field-map.js.map
