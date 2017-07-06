e399eec226b99a7270e1f1bc22a56deb161d5b44
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2017-07-06 10:03:58 +0200

    rename to constraints

--- src/app/datastore/query.ts
+++ src/app/datastore/query.ts
@@ -2,7 +2,7 @@
  * represents a query to the datastore
  * @property q the query string that is used to search documents in the datastore
  * @property type restricts results to the given type
- * @property contraint restricts the search result further to match some fields.
+ * @property contraints restricts the search result further to match some fields.
  *   it can be interpreted as additional AND queries. A given contraint of
  *   { 'resource.relations.isRecordedIn' : 'id1' } would mean that the search result
  *   contains the results which match the other properties of the query and which
@@ -14,5 +14,5 @@ export interface Query {
     q: string;
     type?: string;
     prefix?: boolean;
-    contraint?: any;
+    contraints?: any;
 }
