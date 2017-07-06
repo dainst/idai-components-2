a37495e721112ffae08f4a44a85f9053bc4a35c4
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2017-07-06 11:55:11 +0200

    remove synced property

--- src/app/idai-field-model/idai-field-document.ts
+++ src/app/idai-field-model/idai-field-document.ts
@@ -2,8 +2,6 @@ import {Document} from '../model/document';
 import {IdaiFieldResource} from './idai-field-resource';
 
 export interface IdaiFieldDocument extends Document {
-
-    synced: number;
     resource: IdaiFieldResource;
     id?: string;
 }
