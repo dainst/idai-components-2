bfd1d0069f5a93d270a2caaab190a8cb1c5fe4c4
Thomas Kleinke <thomas.kleinke@dainst.de>
2017-07-19 20:25:12 +0200

    export geometry interfaces

--- idai-field-map.ts
+++ idai-field-map.ts
@@ -1,2 +1,5 @@
 export {IdaiFieldMapModule} from './src/app/idai-field-map/idai-field-map.module';
 export {MapComponent} from './src/app/idai-field-map/map.component';
+export {IdaiFieldPolygon} from './src/app/idai-field-map/idai-field-polygon';
+export {IdaiFieldPolyline} from './src/app/idai-field-map/idai-field-polyline';
+export {IdaiFieldMarker} from './src/app/idai-field-map/idai-field-marker';
