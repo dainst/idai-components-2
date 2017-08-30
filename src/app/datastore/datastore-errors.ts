ab678bdeb9e61055f5686545fb7c78054f898d04
Thomas Kleinke <thomas.kleinke@dainst.de>
2017-08-30 16:09:58 +0200

    minor changes

--- src/app/datastore/datastore-errors.ts
+++ src/app/datastore/datastore-errors.ts
@@ -3,10 +3,9 @@
  */
 export class DatastoreErrors {
 
-    public static GENERIC_ERROR : string = 'idai-components-2/datastore/genericerror';
-    public static DOCUMENT_RESOURCE_ID_EXISTS : string = 'idai-components-2/datastore/documentresourceidexists';
-    public static DOCUMENT_NOT_FOUND : string = 'idai-components-2/datastore/documentnotfound';
-
-    public static SAVE_CONFLICT : string = 'idai-components-2/datastore/saveconflict';
-    public static DOCUMENT_NO_RESOURCE_ID : string = 'idai-components-2/datastore/documentnoresourceid';
+    public static GENERIC_ERROR: string = 'idai-components-2/datastore/genericerror';
+    public static DOCUMENT_RESOURCE_ID_EXISTS: string = 'idai-components-2/datastore/documentresourceidexists';
+    public static DOCUMENT_NO_RESOURCE_ID: string = 'idai-components-2/datastore/documentnoresourceid';
+    public static DOCUMENT_NOT_FOUND: string = 'idai-components-2/datastore/documentnotfound';
+    public static SAVE_CONFLICT: string = 'idai-components-2/datastore/saveconflict';
 }
