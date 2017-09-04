ce8e54321ca35df8e0481bbe769ead52dd422666
Thomas Kleinke <thomas.kleinke@dainst.de>
2017-09-04 17:26:34 +0200

    fix document view demo

--- demo/app/document-view-demo.component.ts
+++ demo/app/document-view-demo.component.ts
@@ -1,10 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {ConfigLoader} from '../../src/app/configuration/config-loader';
+import {FieldDefinition} from '../../src/app/configuration/field-definition';
 import {IdaiType} from '../../src/app/configuration/idai-type';
 import {Datastore} from '../../src/app/datastore/datastore';
-
-import {Document} from "../../src/app/model/document";
-import {FieldDefinition} from "../../src/app/configuration/field-definition";
+import {Document} from '../../src/app/model/document';
 
 @Component({
     selector: 'document-edit-demo',
@@ -13,40 +12,18 @@ import {FieldDefinition} from "../../src/app/configuration/field-definition";
 export class DocumentViewDemoComponent implements OnInit {
 
     private documents = new Array();
-    private selectedDocument : Document;
-    private fieldDefinitions : Array<FieldDefinition>;
+    private selectedDocument: Document;
+    private fieldDefinitions: Array<FieldDefinition>;
 
     private types : IdaiType[];
 
     constructor(
-        private configLoader:ConfigLoader,
+        private configLoader: ConfigLoader,
         private datastore: Datastore) {
     }
 
-    public clicked(id) {
-        this.changeTo(id);
-    }
-
-    private changeTo(id) {
-        this.configLoader.getProjectConfiguration().then(projectConfiguration=>{
-            this.datastore.get(id).then((document)=> {
-                this.selectedDocument = JSON.parse(JSON.stringify(document));
-                this.fieldDefinitions = projectConfiguration.getFieldDefinitions(this.selectedDocument.resource.type);
-            });
-        });
-
-    }
-
-    private startEdit(doc: Document) {
-        alert("User wants to edit the document");
-    }
-
-    private deselect() {
-        this.selectedDocument = undefined;
-    }
-
-
     ngOnInit() {
+
         this.configLoader.getProjectConfiguration().then(projectConfiguration=>{
             this.types = projectConfiguration.getTypesTreeList();
         });
@@ -54,5 +31,30 @@ export class DocumentViewDemoComponent implements OnInit {
         this.datastore.find({q:''}).then(docs => {
             this.documents = docs as Document[];
         })
+    }
+
+    public clicked(id) {
+
+        this.changeTo(id);
+    }
+
+    private changeTo(id) {
+
+        this.configLoader.getProjectConfiguration().then(projectConfiguration => {
+            this.datastore.get(id).then(document => {
+                this.selectedDocument = JSON.parse(JSON.stringify(document));
+                this.fieldDefinitions = projectConfiguration.getFieldDefinitions(this.selectedDocument.resource.type);
+            });
+        });
+    }
+
+    public startEdit(doc: Document) {
+
+        alert("User wants to edit the document");
+    }
+
+    public deselect() {
+
+        this.selectedDocument = undefined;
     }
 }
