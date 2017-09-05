20a44c733ebf3a562be5767a48dfbaf194cafd1e
Thomas Kleinke <thomas.kleinke@dainst.de>
2017-09-05 19:29:38 +0200

    validator test adjustments

--- test/browser/persist/validator.spec.ts
+++ test/browser/persist/validator.spec.ts
@@ -1,7 +1,7 @@
-import {MDInternal} from "../../../src/app/messages/md-internal";
-import {Validator} from "../../../src/app/persist/validator";
-import {ProjectConfiguration} from "../../../src/app/configuration/project-configuration";
-import {ConfigLoader} from "../../../src/app/configuration/config-loader";
+import {MDInternal} from '../../../src/app/messages/md-internal';
+import {Validator} from '../../../src/app/persist/validator';
+import {ProjectConfiguration} from '../../../src/app/configuration/project-configuration';
+import {ConfigLoader} from '../../../src/app/configuration/config-loader';
 
 
 /**
@@ -12,148 +12,134 @@ export function main() {
 
         var projectConfiguration = new ProjectConfiguration(
             {
-                "types" : [
+                types: [
                     {
-                        "type" : "T",
-                        "fields" : [
+                        type: 'T',
+                        fields: [
                             {
-                                "name" : "id",
+                                name: 'id',
                             },
                             {
-                                "name" : "type",
+                                name: 'type',
                             },
                             {
-                                "name" : "optional",
+                                name: 'optional',
                             },
                             {
-                                "name" : "mandatory",
-                                "mandatory" : true
+                                name: 'mandatory',
+                                mandatory: true
                             }
                         ]
                     }
                 ],
-                "relations" : [
-
-                ]
+                relations: {}
             }
         );
 
         var configLoader = {
             getProjectConfiguration: function() {
-                return new Promise<any>((resolve)=>{
-                    resolve(projectConfiguration);
-                });
+                return new Promise<any>(resolve => resolve(projectConfiguration));
             }
-
         };
 
-        beforeEach(
-            function(){
-            }
-        );
+        it('should report nothing', done => {
+            const doc = {
+                resource: {
+                    id: '1',
+                    type: 'T',
+                    mandatory: 'm',
+                    relations: {},
+                }
+            };
+            new Validator(<ConfigLoader> configLoader)
+                .validate(doc).then(() => done(), msgWithParams => fail(msgWithParams));
+        });
 
-        it('should report nothing',
-            function(done){
-                var doc = {
-                    "resource" : {
-                        "id" : "1",
-                        "type" : "T",
-                        "mandatory" : "m",
-                        "relations" : undefined,
-                    }
-                };
-                new Validator(<ConfigLoader>configLoader)
-                    .validate(doc).then(()=>done(),msgWithParams=>fail(msgWithParams));
-            }
-        );
+        it('should report nothing when omitting optional property', done => {
+            const doc = {
+                resource: {
+                    id: '1',
+                    type: 'T',
+                    mandatory: 'm',
+                    relations: {},
+                }
+            };
 
-        it('should report nothing when ommiting optional property',
-            function(done){
-                var doc = {
-                    "resource" : {
-                        "id" : "1",
-                        "type" : "T",
-                        "mandatory" : "m",
-                        "relations" : undefined,
-                    }
-                };
-                new Validator(<ConfigLoader>configLoader)
-                    .validate(doc).then(()=>done(),msgWithParams=>fail(msgWithParams));
-            }
-        );
+            new Validator(<ConfigLoader> configLoader)
+                .validate(doc).then(() => done(), msgWithParams => fail(msgWithParams));
+        });
 
-        it('should report error when ommiting mandatory property',
-            function(done){
-                var doc = {
-                    "resource" : {
-                        "id" : "1",
-                        "type" : "T",
-                        "relations" : undefined,
-                    }
-                };
-                new Validator(<ConfigLoader>configLoader)
-                    .validate(doc).then(()=>fail(),msgWithParams => {
-                    expect(msgWithParams).toEqual([MDInternal.VALIDATION_ERROR_MISSINGPROPERTY,'T','mandatory'])
+        it('should report error when omitting mandatory property', done => {
+
+            const doc = {
+                resource: {
+                    id: '1',
+                    type: 'T',
+                    relations: {},
+                }
+            };
+
+            new Validator(<ConfigLoader> configLoader)
+                .validate(doc).then(() => fail(), msgWithParams => {
+                expect(msgWithParams).toEqual([MDInternal.VALIDATION_ERROR_MISSINGPROPERTY, 'T', 'mandatory']);
+                done();
+            });
+        });
+
+        it('should report error when leaving mandatory property empty', done => {
+
+            const doc = {
+                resource: {
+                    id: '1',
+                    type: 'T',
+                    mandatory: '',
+                    relations: {},
+                }
+            };
+
+            new Validator(<ConfigLoader> configLoader)
+                .validate(doc).then(() => fail(), msgWithParams => {
+                    expect(msgWithParams).toEqual([MDInternal.VALIDATION_ERROR_MISSINGPROPERTY, 'T', 'mandatory']);
                     done();
                 });
-            }
-        );
+        });
 
-        it('should report error when leaving mandatory property empty',
-            function(done){
-                var doc = {
-                    "resource" : {
-                        "id" : "1",
-                        "type" : "T",
-                        "mandatory" : "",
-                        "relations" : undefined,
-                    }
-                };
-                new Validator(<ConfigLoader>configLoader)
-                    .validate(doc).then(()=>fail(),msgWithParams => {
-                        expect(msgWithParams).toEqual([MDInternal.VALIDATION_ERROR_MISSINGPROPERTY,'T','mandatory'])
-                        done();
-                    });
-            }
-        );
+        it('should report a missing field definition', done => {
+            const doc = {
+                resource: {
+                    id: '1',
+                    type: 'T',
+                    a: 'b',
+                    mandatory: 'm',
+                    relations: {},
+                }
+            };
 
-        it('should report a missing field definition',
-            function(done){
-                var doc = {
-                    "resource" : {
-                        "id" : "1",
-                        "type" : "T",
-                        "a" : "b",
-                        "mandatory" : "m",
-                        "relations" : undefined,
-                    }
-                };
-                new Validator(<ConfigLoader>configLoader)
-                    .validate(doc).then(()=>fail(),msgWithParams => {
-                    expect(msgWithParams).toEqual([MDInternal.VALIDATION_ERROR_INVALIDFIELD,'T','a']);
-                    done();
-                });
-            }
-        );
+            new Validator(<ConfigLoader> configLoader)
+                .validate(doc).then(() => fail(), msgWithParams => {
+                expect(msgWithParams).toEqual([MDInternal.VALIDATION_ERROR_INVALIDFIELD, 'T', 'a']);
+                done();
+            });
+        });
 
-        it('should report missing field definitions',
-            function(done){
-                var doc = {
-                    "resource" : {
-                        "id" : "1",
-                        "type" : "T",
-                        "a" : "b",
-                        "b" : "a",
-                        "mandatory" : "m",
-                        "relations" : undefined,
-                    }
-                };
-                new Validator(<ConfigLoader>configLoader)
-                    .validate(doc).then(()=>fail(),msgWithParams => {
-                    expect(msgWithParams).toEqual([MDInternal.VALIDATION_ERROR_INVALIDFIELDS,'T','a, b']);
-                    done();
-                });
-            }
-        );
+        it('should report missing field definitions', done => {
+
+            const doc = {
+                resource: {
+                    id: '1',
+                    type: 'T',
+                    a: 'b',
+                    b: 'a',
+                    mandatory: 'm',
+                    relations: {},
+                }
+            };
+
+            new Validator(<ConfigLoader> configLoader)
+                .validate(doc).then(() => fail(), msgWithParams => {
+                expect(msgWithParams).toEqual([MDInternal.VALIDATION_ERROR_INVALIDFIELDS, 'T', 'a, b']);
+                done();
+            });
+        });
     })
 }
