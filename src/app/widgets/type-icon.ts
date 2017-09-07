0213bdc5aaf9f2f93d6f122b06aef9e48d29d2f8
Sebastian Cuy <sebastian.cuy@dainst.de>
2017-09-07 19:21:35 +0200

    use black character in type icons if background is too bright

--- src/app/widgets/type-icon.ts
+++ src/app/widgets/type-icon.ts
@@ -4,7 +4,7 @@ import {ConfigLoader} from '../configuration/config-loader';
 @Component({
   selector: 'type-icon',
   template: '<div class="type-icon" [style.width]="pxSize" [style.height]="pxSize" [style.font-size]="pxSize" [style.line-height]="pxSize" [style.background-color]="color">' +
-    '<span class="character">{{character}}</span>' +
+    '<span class="character" [style.color]="textColor">{{character}}</span>' +
   '</div>'
 })
 
@@ -19,6 +19,7 @@ export class TypeIconComponent implements OnChanges {
 
   private character: string;
   private color: string;
+  private textColor: string;
   private pxSize: string;
 
   constructor(private configLoader: ConfigLoader) { }
@@ -28,8 +29,20 @@ export class TypeIconComponent implements OnChanges {
     this.configLoader.getProjectConfiguration().then(config => {
       this.character = config.getLabelForType(this.type).substr(0, 1);
       this.color = config.getColorForType(this.type);
+      this.textColor = this.isColorTooBright(this.color) ? 'black' : 'white';
       this.pxSize = this.size + 'px';
     });
+  }
+
+  private isColorTooBright(c): boolean {
+
+    c = c.substring(1);      // strip #
+    let rgb = parseInt(c, 16);   // convert rrggbb to decimal
+    let r = (rgb >> 16) & 0xff;  // extract red
+    let g = (rgb >>  8) & 0xff;  // extract green
+    let b = (rgb >>  0) & 0xff;  // extract blue
+    let luma = 0.2126 * r + 0.7152 * g + 0.0722 * b; // per ITU-R BT.709
+    return luma > 200;
   }
 
 }
