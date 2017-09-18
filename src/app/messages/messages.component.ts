405cf907224a16d9915ddce1201fbe88c985d01e
Thomas Kleinke <thomas.kleinke@dainst.de>
2017-09-18 17:18:37 +0200

    adjust message close button style

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,25 +1,15 @@
 import {Component} from '@angular/core';
-import {Messages} from "./messages";
-import {Message} from "./message";
+import {Messages} from './messages';
+import {Message} from './message';
 
 /**
  * @author Jan G. Wieners
  * @author Thomas Kleinke
  */
 @Component({
+    moduleId: module.id,
     selector: 'messages',
-    template: `<ng-container *ngFor="let message of messages.getMessages().reverse(); let index=index">
-        <div *ngIf="message.hidden==false || message.overrideHidden == true"
-            id="message-{{index}}" style="text-align:right;">
-            <div class="alert alert-{{message.level}}" style="display: inline-block;">
-                <button *ngIf="message.level == 'danger' || message.level == 'warning'" type="button" class="close"
-                        (click)="closeAlert(message)" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-                <span class="message-content">{{getMessageContent(message)}}</span>
-            </div>
-        </div>
-    </ng-container>`
+    templateUrl: './messages.html'
 })
 
 export class MessagesComponent {
@@ -27,11 +17,12 @@ export class MessagesComponent {
     constructor(private messages: Messages) {}
     
     public getMessageContent(message: Message): string {
+
         let content = message.content;
 
         if (message.params) {
             for (let i in message.params) {
-                content = content.replace("{" + i + "}", message.params[i]);
+                content = content.replace('{' + i + '}', message.params[i]);
             }
         }
 
@@ -39,6 +30,7 @@ export class MessagesComponent {
     }
 
     public closeAlert(message: Message) {
+
         this.messages.hideMessage(message);
     }
 }
