23a1dd48494c53d7e2e23c362bad2c51443c8cfb
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2017-10-24 20:54:02 +0200

    export relations

--- idai-field-model.ts
+++ idai-field-model.ts
@@ -1,4 +1,5 @@
 export {IdaiFieldAppConfigurator} from './src/app/idai-field-model/idai-field-app-configurator';
 export {IdaiFieldDocument} from './src/app/idai-field-model/idai-field-document';
 export {IdaiFieldResource} from './src/app/idai-field-model/idai-field-resource';
+export {IdaiFieldRelations} from './src/app/idai-field-model/idai-field-relations';
 export {IdaiFieldGeometry} from './src/app/idai-field-model/idai-field-geometry';
