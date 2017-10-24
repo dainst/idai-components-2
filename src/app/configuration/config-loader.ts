f463a112ed6755772dc826b7a90fa110575f72ca
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2017-10-25 00:13:51 +0200

    use async await

--- src/app/configuration/config-loader.ts
+++ src/app/configuration/config-loader.ts
@@ -5,6 +5,7 @@ import {MDInternal} from '../messages/md-internal';
 import {ConfigurationPreprocessor} from './configuration-preprocessor';
 import {ConfigurationValidator} from './configuration-validator';
 
+@Injectable()
 /**
  * Lets clients subscribe for the app
  * configuration. In order for this to work, they
@@ -18,12 +19,25 @@ import {ConfigurationValidator} from './configuration-validator';
  * @author Thomas Kleinke
  * @author Fabian Z.
  */
-@Injectable()
 export class ConfigLoader {
+
+    private static defaultFields = [
+        {
+            name: 'id',
+            editable: false,
+            visible: false
+        },
+        {
+            name: 'type',
+            visible: false,
+            editable: false
+        }
+    ];
 
     private projectConfig: Promise<ProjectConfiguration>|undefined = undefined;
     private projectConfigResolveFunction: Function|undefined = undefined;
     private projectConfigRejectFunction: Function|undefined = undefined;
+
 
     constructor(private http: Http) {
 
@@ -48,39 +62,26 @@ export class ConfigLoader {
      * @param configurationPreprocessor
      * @param configurationValidator
      */
-    public go(projectConfigurationPath: string, configurationPreprocessor: ConfigurationPreprocessor,
+    public async go(projectConfigurationPath: string, configurationPreprocessor: ConfigurationPreprocessor,
               configurationValidator: ConfigurationValidator) {
 
-        const defaultFields = [
-            {
-                name: 'id',
-                editable: false,
-                visible: false
-            },
-            {
-                name: 'type',
-                visible: false,
-                editable: false
-            }
-        ];
+        let config;
+        try {
+            config = await this.read(this.http, projectConfigurationPath);
+        } catch (msgWithParams) {
+            if (this.projectConfigRejectFunction) return this.projectConfigRejectFunction([msgWithParams]);
+        }
 
-        this.read(this.http, projectConfigurationPath).then(
-            config => {
-                if (configurationPreprocessor) configurationPreprocessor.go(config);
-                new ConfigurationPreprocessor([], defaultFields, []).go(config);
+        if (configurationPreprocessor) configurationPreprocessor.go(config);
+        new ConfigurationPreprocessor([], ConfigLoader.defaultFields, []).go(config);
 
-                let configurationErrors: any = [];
-                if (configurationValidator) configurationErrors = configurationValidator.go(config);
-                if (configurationErrors.length > 0) {
-                    if (this.projectConfigRejectFunction) this.projectConfigRejectFunction(configurationErrors);
-                } else {
-                    if (this.projectConfigResolveFunction) this.projectConfigResolveFunction(new ProjectConfiguration(config));
-                }
-            },
-            msgWithParams => {
-                if (this.projectConfigRejectFunction) this.projectConfigRejectFunction([msgWithParams]);
-            }
-        );
+        let configurationErrors: any = [];
+        if (configurationValidator) configurationErrors = configurationValidator.go(config);
+        if (configurationErrors.length > 0) {
+            if (this.projectConfigRejectFunction) this.projectConfigRejectFunction(configurationErrors);
+        } else {
+            if (this.projectConfigResolveFunction) this.projectConfigResolveFunction(new ProjectConfiguration(config));
+        }
     }
 
     private read(http: any, path: string): Promise<any> {
