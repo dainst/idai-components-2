97c7bedae6d528af07df99e8d837c5c134cb8e18
Thomas Kleinke <thomas.kleinke@dainst.de>
2017-10-30 17:38:46 +0100

    adjust dating component to code style rules

--- src/app/documents/docedit/forms/dating.component.ts
+++ src/app/documents/docedit/forms/dating.component.ts
@@ -1,6 +1,6 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {Resource} from "../../../model/resource";
-import {DocumentEditChangeMonitor} from "../document-edit-change-monitor";
+import {Component, Input} from '@angular/core';
+import {Resource} from '../../../model/resource';
+import {DocumentEditChangeMonitor} from '../document-edit-change-monitor';
 
 /**
  * @author Sebastian Cuy
@@ -15,47 +15,55 @@ export class DatingComponent {
 
     // TODO: use this map in template as well
     public DATE_TYPES = {
-        "bce": "v.Chr.",
-        "ce": "n.Chr.",
-        "bp": "BP"
-    }
+        'bce': 'v.Chr.',
+        'ce': 'n.Chr.',
+        'bp': 'BP'
+    };
 
     @Input() resource: Resource;
     @Input() field: any;
 
     public newDating: {} = null;
 
+
     constructor(private documentEditChangeMonitor: DocumentEditChangeMonitor) {}
 
-    public removeDating(i:number): void {
-        this.resource[this.field.name].splice(i, 1);
+
+    public removeDating(index: number) {
+
+        this.resource[this.field.name].splice(index, 1);
     }
 
-    public createNewDating(): void {
+
+    public createNewDating() {
+
         this.newDating = {
             type: 'range',
             dates: [{ value: 0, type: 'bce' }, { value: 0, type: 'bce' }]
         };
     }
 
-    public addNewDating(): void {
-        if (!this.resource[this.field.name])
-            this.resource[this.field.name] = [];
+
+    public addNewDating() {
+
+        if (!this.resource[this.field.name]) this.resource[this.field.name] = [];
         this.resource[this.field.name].push(this.convertDating(this.newDating));
         this.newDating = null;
     }
+
 
     public convertDating(dating): any {
 
         for (let date of dating.dates) {
             if (date.value < 0) return false;
         }
-        let converted = this.createNormalizedDating(dating);
-        if (dating.type != 'scientific'
-                && converted['hasBegin']
-                && converted['hasEnd'])
-            if (converted['hasBegin']['year'] > converted['hasEnd']['year'])
-                return false;
+
+        const converted = this.createNormalizedDating(dating);
+
+        if (dating.type != 'scientific' && converted['hasBegin'] && converted['hasEnd']
+                && converted['hasBegin']['year'] > converted['hasEnd']['year']) {
+            return false;
+        }
 
         if (dating.type == 'scientific' && dating.margin > 0) {
             converted['hasEnd']['year'] = converted['hasBegin']['year'] + dating.margin;
@@ -71,22 +79,30 @@ export class DatingComponent {
         return converted;
     }
 
+
     private createNormalizedDating(dating) {
-        let normalized = {};
+
+        const normalized = {};
+
         if (dating.type != 'before')
             normalized['hasBegin'] = { year: this.normalizeDate(dating.dates[0]) };
         if (dating.type != 'after')
             normalized['hasEnd'] = { year: this.normalizeDate(dating.dates[1]) };
         if (dating.type == 'exact')
             normalized['hasEnd'] = { year: this.normalizeDate(dating.dates[0]) };
+
         return normalized;
     }
 
+
     private normalizeDate(date) {
+
         if (date.type == 'bce') return 0 - date.value;
         if (date.type == 'bp') return 1950 - date.value;
+
         return date.value;
     }
+
 
     private generateLabel(dating): string {
 
@@ -94,17 +110,15 @@ export class DatingComponent {
         let year = '';
         let postfix = '';
 
-        if (dating.type == 'range')
+        if (dating.type == 'range') {
             year = this.generateLabelForDate(dating.dates[0])
                 + ' – ' + this.generateLabelForDate(dating.dates[1]);
-        if (dating.type == 'before')
-            year = this.generateLabelForDate(dating.dates[1]);
-        if (dating.type == 'after' || dating.type == 'exact')
-            year = this.generateLabelForDate(dating.dates[0]);
+        }
+        if (dating.type == 'before') year = this.generateLabelForDate(dating.dates[1]);
+        if (dating.type == 'after' || dating.type == 'exact') year = this.generateLabelForDate(dating.dates[0]);
         if (dating.type == 'scientific') {
             year = this.generateLabelForDate(dating.dates[0]);
-            if (dating.margin > 0)
-                year += ' ± ' + dating.margin;
+            if (dating.margin > 0) year += ' ± ' + dating.margin;
         }
 
         if (dating['isImprecise']) prefix = 'ca. ';
@@ -118,9 +132,13 @@ export class DatingComponent {
         return prefix + year + postfix;
     }
 
-    private generateLabelForDate(date): string {
-        if (date.value == 0) return "0";
-        else return date.value + " " + this.DATE_TYPES[date.type];
-    }
 
+    private generateLabelForDate(date): string {
+
+        if (date.value == 0) {
+            return '0';
+        } else {
+            return date.value + ' ' + this.DATE_TYPES[date.type];
+        }
+    }
 }
