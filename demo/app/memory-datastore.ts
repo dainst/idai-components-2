0b0900d27eba89f6ad60bd9c032d20b379b2f4c0
Thomas Kleinke <thomas.kleinke@dainst.de>
2017-12-04 15:43:48 +0100

    adjust memory datastore to code style rules

--- demo/app/memory-datastore.ts
+++ demo/app/memory-datastore.ts
@@ -13,50 +13,68 @@ export class MemoryDatastore implements Datastore {
     private db: Promise<any>;
     private objectCache: { [id: string]: Document } = {};
 
+
+    constructor() {};
+
+
     public getUnsyncedObjects(): Observable<Document> {
+
         return undefined;
     }
-    
-    constructor() {};
+
 
     // NOT IMPLEMENTED
     public remoteChangesNotifications(): Observable<Document> {
+
         return undefined;
     }
 
+
     public create(document: Document): Promise<Document> {
+
         this.objectCache[document.resource.id] = document;
         return Promise.resolve(document);
     }
+
 
     public update(document: Document): Promise<Document> {
+
         this.objectCache[document.resource.id] = document;
         return Promise.resolve(document);
     }
 
-    public refresh(document: Document): Promise<Document>  {
+
+    public refresh(document: Document): Promise<Document> {
+
         return Promise.resolve(undefined);
     }
 
+
     public get(id: string): Promise<Document> {
+
         return new Promise<Document>((resolve, reject) => {
             if (!this.objectCache[id]) reject('document not found');
             resolve(this.objectCache[id]);
         });
     }
 
+
     public remove(document: Document): Promise<any> {
+
         return Promise.resolve();
     }
 
+
     public clear(): Promise<any> {
+
         return new Promise((resolve, reject) => {
             this.db.then(db => {
                 resolve();
             });
         });
     }
-    
+
+
     public find(query: Query): Promise<Document[]> {
 
         if (!query.q) query.q = '';
@@ -71,6 +89,7 @@ export class MemoryDatastore implements Datastore {
             resolve(results);
         });
     }
+
 
     public all(): Promise<Document[]> {
 
