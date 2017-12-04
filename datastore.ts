5c5268d949bd238fa13d1745d86e3bec15112c35
Thomas Kleinke <thomas.kleinke@dainst.de>
2017-12-04 16:09:10 +0100

    export FindResult

--- datastore.ts
+++ datastore.ts
@@ -1,4 +1,4 @@
 export {Query} from './src/app/datastore/query';
 export {Datastore} from './src/app/datastore/datastore';
-export {ReadDatastore} from './src/app/datastore/read-datastore';
+export {ReadDatastore, FindResult} from './src/app/datastore/read-datastore';
 export {DatastoreErrors} from './src/app/datastore/datastore-errors';
