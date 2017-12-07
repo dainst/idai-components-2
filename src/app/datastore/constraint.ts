53ee8f2686161930a9faabefd70b44679d264a07
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2017-12-07 13:55:54 +0100

    Make companion objects

--- src/app/datastore/constraint.ts
+++ src/app/datastore/constraint.ts
@@ -2,3 +2,16 @@ export interface Constraint {
     value: string;
     type: string;   // add | subtract
 }
+
+
+/**
+ * Companion object
+ */
+export class Constraint {
+
+    public static convertTo(constraint: Constraint|string): Constraint {
+
+        return (typeof(constraint) == 'string') ?
+            {value: constraint, type: 'add' } : constraint;
+    }
+}
--- src/app/model/document.ts
+++ src/app/model/document.ts
@@ -6,3 +6,20 @@ export interface Document {
     modified?: Action[];
     created?: Action;
 }
+
+
+/**
+ * Companion object
+ */
+export class Document {
+
+    public static isValid(document: Document): boolean {
+
+        if (!document.resource) return false;
+        if (!document.resource.id) return false;
+        if (!document.resource.relations) return false;
+        if (!document.created) return false;
+
+        return true;
+    }
+}
