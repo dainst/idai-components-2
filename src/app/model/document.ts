4d4c90b41ee94a8f1cd153cd337b5afff2fdf511
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2018-02-15 20:49:30 +0000

    Prepare remove methods to use with flow

--- src/app/model/document.ts
+++ src/app/model/document.ts
@@ -26,18 +26,20 @@ export class Document {
     }
 
 
-    public static removeFields<D extends Document>(document: D, fields: Array<string>): D {
+    public static removeFields = <D extends Document>(fields: Array<string>) =>
+        (document: D): D => {
 
-        const result = subtractO([])(document) as D; // TODO implement shallow copy method
-        result.resource = subtractO(fields)(document.resource) as Resource;
-        return result;
-    }
+            const result = subtractO([])(document) as D; // TODO implement shallow copy method
+            result.resource = subtractO(fields)(document.resource) as Resource;
+            return result;
+        };
 
 
-    public static removeRelations<D extends Document>(document: D, relations: Array<string>): D {
+    public static removeRelations = <D extends Document>(relations: Array<string>) =>
+        (document: D): D => {
 
-        const result = subtractO([])(document) as D; // TODO implement shallow copy method
-        result.resource.relations = subtractO(relations)(result.resource.relations);
-        return result;
-    }
+            const result = subtractO([])(document) as D; // TODO implement shallow copy method
+            result.resource.relations = subtractO(relations)(result.resource.relations);
+            return result;
+        };
 }
