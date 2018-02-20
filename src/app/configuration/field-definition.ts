7de8bc28ffc603f0da07dcd1448519bcfa5eb4bc
Thomas Kleinke <thomas.kleinke@dainst.de>
2018-02-20 14:12:52 +0100

    add inputType to FieldDefinition

--- src/app/configuration/field-definition.ts
+++ src/app/configuration/field-definition.ts
@@ -3,10 +3,11 @@
  */
 export interface FieldDefinition {
 
-    label? : string;
+    label?: string;
     name: string;
     description?: string;
-    editable? : boolean;  // defaults to true
-    visible? : boolean;   // defaults to true
-    mandatory? : boolean; // defaults to false
+    inputType?: string;
+    editable?: boolean;  // defaults to true
+    visible?: boolean;   // defaults to true
+    mandatory?: boolean; // defaults to false
 }
