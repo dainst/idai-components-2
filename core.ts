0f45f066d9b46df8957578785d0a5e8b6eac9601
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2018-03-07 01:56:47 +0000

    Add exports

--- core.ts
+++ core.ts
@@ -1,5 +1,7 @@
 // Model
 export {Document} from './src/app/model/document';
+export {NewDocument} from './src/app/model/new-document';
 export {Resource} from './src/app/model/resource';
+export {NewResource} from './src/app/model/new-resource';
 export {Relations} from './src/app/model/relations';
 export {Action} from './src/app/model/action';
