5baa2306245e21f64f852eccebaa7a81cb0a42b6
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2018-04-26 14:23:29 +0100

    Make modified and created non optional in document

--- src/core/model/document.ts
+++ src/core/model/document.ts
@@ -2,20 +2,23 @@ import {Resource} from './resource';
 import {copy} from 'tsfun';
 import {subtract} from 'tsfun/objects';
 import {NewDocument} from "./new-document";
+import {Action} from './action';
 
 
 export interface Document extends NewDocument {
 
     resource : Resource;
+    modified: Action[];
+    created: Action;
 }
 
 
 /**
  * Companion object
  */
-export class Document {
+export module Document {
 
-    public static isValid(document: Document, missingIdLegal = false): boolean {
+    export function isValid(document: Document, missingIdLegal = false): boolean {
 
         if (!document.resource) return false;
         if (!document.resource.id && !missingIdLegal) return false;
@@ -26,20 +29,22 @@ export class Document {
     }
 
 
-    public static removeFields = <D extends Document>(fields: Array<string>) =>
-        (document: D): D => {
+    export function removeFields<D extends Document>(fields: Array<string>) {
+        return (document: D): D => {
 
             const result = copy(document);
             result.resource = subtract(fields)(document.resource) as Resource;
             return result as D;
         };
+    }
 
 
-    public static removeRelations = <D extends Document>(relations: Array<string>) =>
-        (document: D): D => {
+    export function removeRelations<D extends Document>(relations: Array<string>) {
+        return (document: D): D => {
 
             const result = copy(document);
             result.resource.relations = subtract(relations)(result.resource.relations);
             return result as D;
         };
+    }
 }
