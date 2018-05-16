cfd4de8e39a3aa6594f233f978edfe6da4356474
Thomas Kleinke <thomas.kleinke@dainst.de>
2018-05-16 23:52:46 +0200

    remove unused interface

--- demo/app/extended-resource.ts
+++
@@ -1,6 +0,0 @@
-import {Resource} from "../../src/core/model/resource";
-
-export interface ExtendedResource extends Resource {
-    fieldlist_example? : Array<any>;
-    localized_fieldlist_example? : any;
-}
