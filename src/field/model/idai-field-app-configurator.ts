348052c7f7936a3000ce57bc1ebbbd7e04fd4fcf
daniel <danielmarreirosdeoliveira@googlemail.com>
2018-07-23 20:22:03 +0200

    Adjust allowed types

--- src/field/model/idai-field-app-configurator.ts
+++ src/field/model/idai-field-app-configurator.ts
@@ -29,7 +29,7 @@ export class IdaiFieldAppConfigurator {
         Feature: {
             fields: {
                 hasPeriod: {
-                    inputType: "dropdown-range"
+                    inputType: "dropdownRange"
                 }
             }
         } as TypeDefinition,
