04d6e79a73cc04ce1a5908161e9659668cf524b5
Thomas Kleinke <thomas.kleinke@dainst.de>
2018-10-08 13:30:37 +0200

    adjust to code style rules

--- test/unit/core/messages/messages.spec.ts
+++ test/unit/core/messages/messages.spec.ts
@@ -1,5 +1,5 @@
-import {Messages} from "../../../../src/messages/messages";
-import {MDInternal} from "../../../../src/messages/md-internal";
+import {Messages} from '../../../../src/messages/messages';
+import {MDInternal} from '../../../../src/messages/md-internal';
 
 /**
  * @author Daniel de Oliveira
@@ -9,13 +9,13 @@ import {MDInternal} from "../../../../src/messages/md-internal";
 
 let messagesDictionary = {
     msgs: {
-        "key1" : {
+        'key1' : {
             content: 'content1',
             level: 'danger',
             params: new Array(),
             hidden: false
         },
-        "key2" : {
+        'key2' : {
             content: 'content2',
             level: 'danger',
             params: new Array(),
@@ -24,102 +24,102 @@ let messagesDictionary = {
     }
 };
 
-function verifyUnknownError(consoleError) {
-    expect(messages.getActiveMessages()[1].content).toEqual((new MDInternal().msgs[MDInternal.UNKOWN_ERROR]).content);
+const verifyUnknownError = (consoleError) => {
+
+    expect(messages.getActiveMessages()[1].content).toEqual(
+        (new MDInternal().msgs[MDInternal.UNKOWN_ERROR]).content
+    );
     expect(console.error).toHaveBeenCalledWith(consoleError);
-}
+};
 
 let messages: Messages;
 
-beforeEach(
-    function(){
-        spyOn(console, 'error');
-        messages = new Messages(messagesDictionary, 100);
-        messages.add(["key1"]);
-    }
-);
+
+beforeEach(() => {
+
+    spyOn(console, 'error');
+    messages = new Messages(messagesDictionary, 100);
+    messages.add(['key1']);
+});
+
 
 describe('Messages', () => {
 
+    it('should store, retrieve and delete a message', () => {
 
-    it('should store, retrieve and delete a message',
-        function(){
-            expect(messages.getActiveMessages()[0]).toEqual(messagesDictionary.msgs["key1"]);
-            messages.removeAllMessages();
-            expect(messages.getActiveMessages()[0]).toBe(undefined);
-        }
-    );
-
-    it('should add message with same identifier twice',
-        function(){
-            messages.add(["key1"]);
-            expect(messages.getActiveMessages()[0]).toEqual(messagesDictionary.msgs["key1"]);
-            expect(messages.getActiveMessages().length).toBe(2);
-        }
-    );
-
-    it('should add two messages with different identifiers',
-        function(){
-            messages.add(["key2"]);
-            expect(messages.getActiveMessages()[0]).toEqual(messagesDictionary.msgs["key1"]);
-            expect(messages.getActiveMessages()[1]).toEqual(messagesDictionary.msgs["key2"]);
-            expect(messages.getActiveMessages().length).toBe(2);
-        }
-    );
+        expect(messages.getActiveMessages()[0]).toEqual(messagesDictionary.msgs['key1']);
+        messages.removeAllMessages();
+        expect(messages.getActiveMessages()[0]).toBe(undefined);
+    });
 
 
-    it('should return always the same instance',
-        function(){
-            expect(messages.getActiveMessages()==messages.getActiveMessages()).toBeTruthy();
-        }
-    );
+    it('should add message with same identifier twice', () => {
 
-    it('should clear all messages',
-        function(){
-            messages.removeAllMessages();
-            expect(messages.getActiveMessages().length).toBe(0);
-        }
-    );
+        messages.add(['key1']);
+        expect(messages.getActiveMessages()[0]).toEqual(messagesDictionary.msgs['key1']);
+        expect(messages.getActiveMessages().length).toBe(2);
+    });
 
-    it('should show a msg from the internal message dictionary',
-        function(){
-            messages.add([MDInternal.MESSAGES_NOBODY]);
-            expect(messages.getActiveMessages()[1]).toEqual((new MDInternal()).msgs[MDInternal.MESSAGES_NOBODY]);
-        }
-    );
 
-    it('should override a msg from the internal message dictionary with the provided one',
-        function(){
-            messagesDictionary.msgs[MDInternal.MESSAGES_NOBODY]={
-                content: 'test',
-                level: 'danger',
-                params: new Array(),
-                hidden: false
-            };
-            messages.add([MDInternal.MESSAGES_NOBODY]);
-            expect(messages.getActiveMessages()[1]).toEqual(messagesDictionary.msgs[MDInternal.MESSAGES_NOBODY]);
-        }
-    );
+    it('should add two messages with different identifiers', () => {
 
-    it('should add a message with parameters',
-        function(){
-            let params = ['param1','param2'];
-            messages.add(['key2'].concat(params));
-            expect(messages.getActiveMessages()[1].params).toEqual(params);
-        }
-    );
+        messages.add(['key2']);
+        expect(messages.getActiveMessages()[0]).toEqual(messagesDictionary.msgs['key1']);
+        expect(messages.getActiveMessages()[1]).toEqual(messagesDictionary.msgs['key2']);
+        expect(messages.getActiveMessages().length).toBe(2);
+    });
 
-    it('should throw an error if adding a message with parameters but id not found',
-        function(){
-            messages.add(['nonexisting']);
-            verifyUnknownError('no msg found for key of M with id: "nonexisting"');
-        }
-    );
 
-    it('should throw an error if array is empty',
-        function(){
-            messages.add([]);
-            verifyUnknownError('no msg found for key of M with id: "undefined"');
-        }
-    );
+    it('should return always the same instance', () => {
+
+        expect(messages.getActiveMessages() === messages.getActiveMessages()).toBeTruthy();
+    });
+
+
+    it('should clear all messages', () => {
+
+        messages.removeAllMessages();
+        expect(messages.getActiveMessages().length).toBe(0);
+    });
+
+
+    it('should show a msg from the internal message dictionary', () => {
+
+        messages.add([MDInternal.MESSAGES_NOBODY]);
+        expect(messages.getActiveMessages()[1]).toEqual((new MDInternal()).msgs[MDInternal.MESSAGES_NOBODY]);
+    });
+
+
+    it('should override a msg from the internal message dictionary with the provided one', () => {
+
+        messagesDictionary.msgs[MDInternal.MESSAGES_NOBODY]={
+            content: 'test',
+            level: 'danger',
+            params: [],
+            hidden: false
+        };
+        messages.add([MDInternal.MESSAGES_NOBODY]);
+        expect(messages.getActiveMessages()[1]).toEqual(messagesDictionary.msgs[MDInternal.MESSAGES_NOBODY]);
+    });
+
+    it('should add a message with parameters', () => {
+
+        let params = ['param1','param2'];
+        messages.add(['key2'].concat(params));
+        expect(messages.getActiveMessages()[1].params).toEqual(params);
+    });
+
+
+    it('should throw an error if adding a message with parameters but id not found', () => {
+
+        messages.add(['nonexisting']);
+        verifyUnknownError('no msg found for key of M with id: "nonexisting"');
+    });
+
+
+    it('should throw an error if array is empty', () => {
+
+        messages.add([]);
+        verifyUnknownError('no msg found for key of M with id: "undefined"');
+    });
 });
