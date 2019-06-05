3c2a865fe8fed8d032609152ec23c9f20e2944ff
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2019-06-05 13:17:28 +0100

    Add Ägypten

--- src/model/app-configurator.ts
+++ src/model/app-configurator.ts
@@ -117,6 +117,7 @@ export class AppConfigurator {
                 'Anatolien',
                 'Antiochia (Antakya)',
                 'Athen',
+                'Ägypten',
                 'Attisch',
                 'Britannien',
                 'Chios',
