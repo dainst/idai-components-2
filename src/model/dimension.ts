e0ea0337b48d40ae50ce0f6f3a7a9fb79cc317a0
Thomas Kleinke <thomas.kleinke@dainst.de>
2019-07-09 16:33:57 +0200

    add interface dimension (moved from idai-field repository)

---
+++ src/model/dimension.ts
@@ -0,0 +1,18 @@
+/**
+ * @author Thomas Kleinke
+ */
+export interface Dimension {
+
+    value?: number;
+    rangeMin?: number;
+    rangeMax?: number;
+    inputValue: number;
+    inputRangeEndValue: number;
+    measurementPosition?: string;
+    measurementComment?: string;
+    inputUnit: 'mm'|'cm'|'m';
+    isImprecise: boolean;
+    isRange: boolean;
+    label?: string; // Deprecated,
+    unitSuffix?: string; // Not used
+}
