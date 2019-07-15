420a1a54755ed564063d6e9fe8ccbcc32cfe7423
Thomas Kleinke <thomas.kleinke@dainst.de>
2019-07-15 15:21:49 +0200

    add comments to dimension interface

--- src/model/dimension.ts
+++ src/model/dimension.ts
@@ -3,15 +3,20 @@
  */
 export interface Dimension {
 
+    // Normalized values (in micrometres), calculated from input values
     value?: number;
     rangeMin?: number;
     rangeMax?: number;
+
+    // Input values as typed in by the user (in mm/cm/m, defined in inputUnit)
     inputValue: number;
     inputRangeEndValue: number;
+    inputUnit: 'mm'|'cm'|'m';
+
     measurementPosition?: string;
     measurementComment?: string;
-    inputUnit: 'mm'|'cm'|'m';
     isImprecise: boolean;
     isRange: boolean;
+
     label?: string; // Deprecated
 }
