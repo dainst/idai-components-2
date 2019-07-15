aabf856b0612eb69ddbcb03c846ddc4f1416551b
Thomas Kleinke <thomas.kleinke@dainst.de>
2019-07-15 15:56:53 +0200

    include fields for normalized values & input values in dating interface

--- src/model/dating.ts
+++ src/model/dating.ts
@@ -4,11 +4,25 @@
 export interface Dating {
 
     type: 'range'|'exact'|'before'|'after'|'scientific',
-    begin?: { year: number, type: 'bce'|'ce'|'bp' },
-    end?: { year: number, type: 'bce'|'ce'|'bp' },
+
+    begin?: DatingElement,
+    end?: DatingElement,
+
     margin?: number,
     source?: string,
     isImprecise?: boolean,
     isUncertain?: boolean,
+
     label?: string  // Deprecated
+}
+
+
+export interface DatingElement {
+
+    // Normalized value calculated from input values
+    year: number;
+
+    // Input values as typed in by the user
+    inputYear: number;
+    inputType: 'bce'|'ce'|'bp';
 }
