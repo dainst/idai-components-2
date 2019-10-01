e3b2753cace9a0b9854d18e633ef7cb3e8027ac9
Thomas Kleinke <thomas.kleinke@dainst.de>
2019-10-01 12:14:39 +0200

    Adjust to code style rules

--- demo/app/app.routing.ts
+++ demo/app/app.routing.ts
@@ -3,8 +3,8 @@ import {ModuleWithProviders}  from '@angular/core';
 import {MessagesDemoComponent} from './messages-demo.component';
 
 const routes: Routes = [
-    {path: '', redirectTo: 'edit', pathMatch: 'full'},
-    {path: 'messages', component: MessagesDemoComponent}
+    { path: '', redirectTo: 'edit', pathMatch: 'full' },
+    { path: 'messages', component: MessagesDemoComponent }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
