7f61513fed2b3dd2e653c76743fce4afa526dc24
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2020-02-18 11:37:16 +0000

    Extend Contraint with searchRecursively boolean

--- src/datastore/constraint.ts
+++ src/datastore/constraint.ts
@@ -1,18 +1,17 @@
 export interface Constraint {
+
     value: string|string[];
     type: string;   // add | subtract
+    searchRecursively?: boolean;
 }
 
 
-/**
- * Companion object
- */
 export class Constraint {
 
     public static convertTo(constraint: Constraint|string|string[]): Constraint {
 
         return (Array.isArray(constraint) || typeof(constraint) == 'string')
-            ? { value: constraint, type: 'add' }
+            ? { value: constraint, type: 'add', searchRecursively: false }
             : constraint;
     }
 }
