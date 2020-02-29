134129d5fa26dfc94663267374b7633f66dab216
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2020-02-29 13:34:40 +0000

    Change type param of constraint to subtract

--- src/datastore/constraint.ts
+++ src/datastore/constraint.ts
@@ -1,30 +1,16 @@
-import {clone} from 'tsfun/src/struct';
-
-
 export interface Constraint {
 
     value: string|string[];
-    type: 'add'|'subtract';
+    subtract?: true
 }
 
 
-/**
- * Companion object
- */
 export module Constraint {
 
     export function convert(constraint: Constraint|string|string[]): Constraint {
 
         return (Array.isArray(constraint) || typeof(constraint) == 'string')
-            ? { value: constraint, type: 'add' }
-            : complete(constraint);
-    }
-
-
-    function complete(constraint_: Constraint) {
-
-        const constraint = clone(constraint_);
-        constraint.type = constraint.type ?? 'add';
-        return constraint;
+            ? { value: constraint }
+            : constraint;
     }
 }
