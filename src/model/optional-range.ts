2a8ce29a14943b92bf20ccda59553a10b698bd27
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2020-08-10 15:00:36 +0200

    Improve validation

--- src/model/optional-range.ts
+++ src/model/optional-range.ts
@@ -14,7 +14,7 @@ export module OptionalRange {
     export function isValid<T>(optionalRange: OptionalRange<T>): boolean {
 
         const keys = Object.keys(optionalRange);
-        if (keys.length === 1 && keys[0] === ENDVALUE) return false;
+        if (keys.length === 1 && keys[0] !== VALUE) return false;
         if (keys.length === 2 && (typeof optionalRange[VALUE] !== typeof optionalRange[ENDVALUE])) return false;
         return true;
     }
