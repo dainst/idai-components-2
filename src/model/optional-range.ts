bf8b3e119b8a7c205e686bfb1f776192433e4ea4
Thomas Kleinke <thomas.kleinke@dainst.de>
2020-08-31 16:53:05 +0200

    Remove unneeded parentheses

--- src/model/optional-range.ts
+++ src/model/optional-range.ts
@@ -16,7 +16,8 @@ export module OptionalRange {
         const keys = Object.keys(optionalRange);
         if (keys.length !== 1 && keys.length !== 2) return false;
         if (keys.length === 1 && keys[0] !== VALUE) return false;
-        if (keys.length === 2 && (typeof optionalRange[VALUE] !== typeof optionalRange[ENDVALUE])) return false;
+        if (keys.length === 2 && typeof optionalRange[VALUE] !== typeof optionalRange[ENDVALUE]) return false;
+
         return true;
     }
 
