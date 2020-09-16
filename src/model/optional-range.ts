0828cfb7c52ac903dc25356a01e710d23bb18701
Thomas Kleinke <thomas.kleinke@dainst.de>
2020-09-16 16:58:01 +0200

    Adjust validation function of OptionalRange

--- src/model/optional-range.ts
+++ src/model/optional-range.ts
@@ -14,9 +14,9 @@ export module OptionalRange {
     export function isValid(optionalRange: OptionalRange): boolean {
 
         const keys = Object.keys(optionalRange);
-        if (keys.length !== 1 && keys.length !== 2) return false;
+        if (keys.length < 1 || keys.length > 2) return false;
         if (keys.length === 1 && keys[0] !== VALUE) return false;
-        if (keys.length === 2 && typeof optionalRange[VALUE] !== typeof optionalRange[ENDVALUE]) return false;
+        if (keys.length === 2 && (!keys.includes(VALUE) || !keys.includes(ENDVALUE))) return false;
 
         return true;
     }
