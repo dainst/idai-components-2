a84fa36a36881a5e80a0e7d1b89b334f079b6ecf
Thomas Kleinke <thomas.kleinke@dainst.de>
2021-03-01 17:13:55 +0100

    Add fields page & figure to Literature

--- src/model/literature.ts
+++ src/model/literature.ts
@@ -5,6 +5,8 @@ export interface Literature {
 
     quotation: string;
     zenonId?: string;
+    page?: string;
+    figure?: string;
 }
 
 
@@ -12,16 +14,30 @@ export module Literature {
 
     export const QUOTATION = 'quotation';
     export const ZENON_ID = 'zenonId';
+    export const PAGE = 'page';
+    export const FIGURE = 'figure';
 
-    const VALID_FIELDS = [QUOTATION, ZENON_ID];
+    const VALID_FIELDS = [QUOTATION, ZENON_ID, PAGE, FIGURE];
+
 
     export function generateLabel(literature: Literature, getTranslation: (key: string) => string): string {
 
-        return literature.quotation + (literature.zenonId
-            ? ' ('
-            + getTranslation('zenonId')
-            + ': ' + literature.zenonId + ')'
-            : '');
+        let additionalInformation: string[] = [];
+
+        if (literature.zenonId) {
+            additionalInformation.push(getTranslation('zenonId') + ': ' + literature.zenonId);
+        }
+        if (literature.page) {
+            additionalInformation.push(getTranslation('page') + ' ' + literature.page);
+        }
+        if (literature.figure) {
+            additionalInformation.push(getTranslation('figure') + ' ' + literature.figure);
+        }
+
+        return literature.quotation + (additionalInformation.length > 0
+            ? '(' + additionalInformation.join(', ') + ')'
+            : ''
+        );
     }
 
 
