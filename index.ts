631deae17d9d3dd8fd363354422f9914cc72e36a
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-03-30 16:15:52 +0200

    Move dating dimension etc.

--- index.ts
+++ index.ts
@@ -7,8 +7,3 @@ export {NewResource} from './src/model/core/new-resource';
 export {Relations} from './src/model/core/relations';
 export {relationsEquivalent} from './src/model/core/relations';
 export {Action} from './src/model/core/action';
-
-export {Dating, DatingElement, DatingType} from './src/model/dating';
-export {Dimension} from './src/model/dimension';
-export {Literature} from './src/model/literature';
-export {OptionalRange} from './src/model/optional-range';
